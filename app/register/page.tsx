1f806b04c9c9428c7d40d4c64be05b7cfe76e5cb
ritik8848 <ritik@8848digital.com>
2025-03-18 13:56:39 +0530

    feat: enhance registration page with password visibility toggle and Chakra UI components

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,51 +1,53 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { axiosInstance } from "@/lib/api/axios";
+import { Button, Card, Icon, IconButton, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 export default function RegisterPage() {
   const [formData, setFormData] = useState({
-      name: '',
-      role: '',
-      email: '',
-      password: '',
-    });
-  
+    name: '',
+    role: '',
+    email: '',
+    password: '',
+  });
+
   const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show)
   const router = useRouter()
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData((prevData) => ({
-          ...prevData,
-          [e.target.name]: e.target.value,
-        }));
-      };
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const handleRegister = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setLoading(true);
-        try {
-          const response = await axiosInstance.post('/api/method/hackathon.API.register_api.register', {
-            email: formData.email,
-            pwd: formData.password,
-            first_name: formData.name,
-            role: formData.role,
-          });
-          
-          if (response.status === 200) {
-            router.push('/login')          
-          } else {
-           console.error(response);
-          }
-        } catch (error) {
-          console.error(error)
-        }
-        setLoading(false);
-      };
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axiosInstance.post('/api/method/hackathon.API.register_api.register', {
+        email: formData.email,
+        pwd: formData.password,
+        first_name: formData.name,
+        role: formData.role,
+      });
+
+      if (response.status === 200) {
+        router.push('/login')
+      } else {
+        console.error(response);
+      }
+    } catch (error) {
+      console.error(error)
+    }
+    setLoading(false);
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-black">
@@ -70,7 +72,7 @@ export default function RegisterPage() {
               className="w-full p-3 border rounded-md"
               value={formData.role}
               onChange={handleChange}
-              // required
+            // required
             />
           </div>
           <Input
@@ -82,19 +84,24 @@ export default function RegisterPage() {
             onChange={handleChange}
             required
           />
-          <Input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="w-full p-3 border rounded-md"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+          <InputGroup>
+            <InputRightElement>
+              <IconButton bg="transparent !important"
+                variant="ghost"
+                tabIndex={-1}
+                colorScheme='gray'
+                aria-label={show ? "Mask password" : "Reveal password"}
+                icon={show ? <AiFillEyeInvisible /> : <AiFillEye />}
+                onClick={handleClick}
+              />
+            </InputRightElement>
+            <Input name="password" rounded='lg' placeholder='Password' type={show ? "text" : "password"} onChange={handleChange} value={formData.password} required/>
+          </InputGroup>
+
           <Button type="submit" className="w-full bg-black text-white p-3 rounded-md" disabled={loading}>
             {loading ? 'Registering...' : 'Register'}
           </Button>
-          
+
         </form>
 
         <p className="mt-4 text-center text-gray-500">
